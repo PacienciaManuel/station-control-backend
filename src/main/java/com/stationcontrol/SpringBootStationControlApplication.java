e9bcf1274119b4414f2431e390e265714b912331
Paciência Isaac Manuel <141887189+PacienciaManuel@users.noreply.github.com>
2024-01-15 18:04:40 +0100

    Configuração do projeto

---
+++ src/main/java/com/stationcontrol/SpringBootStationControlApplication.java
@@ -0,0 +1,13 @@
+package com.stationcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStationControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStationControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stationcontrol/SpringBootStationControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stationcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStationControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
